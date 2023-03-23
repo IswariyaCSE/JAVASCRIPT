969e9e35d1cc711e81dd46a697ebfca98fd0c827
IswariyaCSE <107340178+IswariyaCSE@users.noreply.github.com>
2023-03-23 21:12:02 +0530

    Add files via upload

---
+++ java1.js
@@ -0,0 +1,30 @@
+//JAVASCRIPT DAY 1
+var str="Lets start JavaScript";
+console.log("String=",str);
+/*DATATYPES ARE 
+Undefined,null,boolean,string,symbol,number,object*/
+const pi=3.14;
+console.log("Value of pi =",pi);
+var a=6;
+var b=10; 
+var c="I am a";
+console.log("Value of a =",a+=2);
+console.log("Value of b =",b-=3);
+console.log("Value of c =",c+" Dinosaur");
+var n1=2;
+var n2=5;
+var n3=8;
+console.log("Addition =",n1+n2+n3);
+console.log("Subtraction =",n1-n2);
+console.log("Multiplication =",n1*n3);
+console.log("Division =",n3/n1);
+console.log("Incrementing n1 =",n1++);
+console.log("Decrementing n2 =",n2--);
+var decimal=8.265;
+console.log("Decimal value =",decimal);
+var remainder;
+remainder=11%2;
+console.log("Remainder =",remainder);
+var mystr1="I am a dinosaur."+" I am Big.";
+console.log(mystr1);
+
